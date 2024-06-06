80403f7130cd48e68e802ee3111760256e49c77d
oSumAtrIX <johan.melkonyan1@web.de>
2024-06-06 23:59:06 +0200

    feat: Add rate limiting to routes

--- src/main/kotlin/app/revanced/api/configuration/routing/routes/OldApi.kt
+++ src/main/kotlin/app/revanced/api/configuration/routing/routes/OldApi.kt
@@ -2,15 +2,18 @@ package app.revanced.api.configuration.routing.routes
 
 import app.revanced.api.configuration.services.OldApiService
 import io.ktor.server.application.*
+import io.ktor.server.plugins.ratelimit.*
 import io.ktor.server.routing.*
 import org.koin.ktor.ext.get
 
 internal fun Route.oldApiRoute() {
     val oldApiService = get<OldApiService>()
 
-    route(Regex("/(v2|tools|contributors).*")) {
-        handle {
-            oldApiService.proxy(call)
+    rateLimit(RateLimitName("weak")) {
+        route(Regex("/(v2|tools|contributors).*")) {
+            handle {
+                oldApiService.proxy(call)
+            }
         }
     }
 }
