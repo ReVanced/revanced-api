03adb6335b7599603a88553685bfb5351f6041c7
oSumAtrIX <johan.melkonyan1@web.de>
2024-06-29 04:14:02 +0200

    Add swagger and redoc routes

--- src/main/kotlin/app/revanced/api/configuration/Routing.kt
+++ src/main/kotlin/app/revanced/api/configuration/Routing.kt
@@ -5,21 +5,26 @@ import app.revanced.api.configuration.routes.announcementsRoute
 import app.revanced.api.configuration.routes.oldApiRoute
 import app.revanced.api.configuration.routes.patchesRoute
 import app.revanced.api.configuration.routes.rootRoute
+import io.bkbn.kompendium.core.routes.redoc
+import io.bkbn.kompendium.core.routes.swagger
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
-import org.koin.ktor.ext.get
 import kotlin.time.Duration.Companion.minutes
+import org.koin.ktor.ext.get as koinGet
 
 internal fun Application.configureRouting() = routing {
-    val configuration = get<ConfigurationRepository>()
+    val configuration = koinGet<ConfigurationRepository>()
 
     installCache(5.minutes)
 
     route("/v${configuration.apiVersion}") {
-        rootRoute()
         patchesRoute()
         announcementsRoute()
+        rootRoute()
     }
+
+    swagger(pageTitle = "ReVanced API", path = "/")
+    redoc(pageTitle = "ReVanced API", path = "/redoc")
 
     // TODO: Remove, once migration period from v2 API is over (In 1-2 years).
     oldApiRoute()
