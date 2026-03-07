4e645f6fc3cc8754ddaf74b973862c6946d63e8e
oSumAtrIX <johan.melkonyan1@web.de>
2026-03-07 01:18:28 +0100

    use utc timestamp

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,11 +1,13 @@
 import { sqliteTable, integer, text } from "drizzle-orm/sqlite-core";
 
 export const announcements = sqliteTable("announcements", {
-  id: integer("id").primaryKey({ autoIncrement: true }),
-  author: text("author"),
-  title: text("title").notNull(),
-  content: text("content"),
-  createdAt: text("created_at").notNull(),
-  archivedAt: text("archived_at"),
-  level: integer("level").notNull().default(0),
+	id: integer("id").primaryKey({ autoIncrement: true }),
+	author: text("author"),
+	title: text("title").notNull(),
+	content: text("content"),
+	createdAt: text("created_at")
+		.notNull()
+		.$defaultFn(() => new Date().toISOString()),
+	archivedAt: text("archived_at"),
+	level: integer("level").notNull().default(0),
 });
