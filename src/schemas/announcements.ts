9edbb14fe14a3e9e6ed4b26ef9387ee57f79adf1
oSumAtrIX <johan.melkonyan1@web.de>
2026-03-09 15:59:34 +0100

    fix timestamp

--- src/schemas/announcements.ts
+++ src/schemas/announcements.ts
@@ -17,10 +17,9 @@ export const AnnouncementResponseSchema = z
 		author: z.string().nullable().openapi({ example: "ReVanced" }),
 		title: z.string().openapi({ example: "Welcome" }),
 		content: z.string().nullable().openapi({ example: "Some content" }),
-		created_at: z
-			.string()
+		created_at: z.iso
 			.datetime()
-			.openapi({ example: "2024-01-01T00:00:00.000Z" }),
+			.openapi({ example: "1970-01-01T00:00:00.000Z" }),
 		archived_at: z.string().datetime().nullable().openapi({ example: null }),
 		level: z.number().int().openapi({ example: 0 }),
 	})
@@ -33,17 +32,11 @@ export const CreateAnnouncementBodySchema = z
 		author: z.string().optional().openapi({ example: "ReVanced" }),
 		title: z.string().openapi({ example: "Welcome" }),
 		content: z.string().optional().openapi({ example: "Some content" }),
-		created_at: z
-			.string()
-			.datetime()
-			.nullable()
-			.optional()
-			.openapi({
-				example: "2024-01-01T00:00:00.000Z",
-				description: "UTC timestamp. Defaults to current time if omitted.",
-			}),
-		archived_at: z
-			.string()
+		created_at: z.string().datetime().nullable().optional().openapi({
+			example: "1970-01-01T00:00:00.000Z",
+			description: "UTC timestamp. Defaults to current time if omitted.",
+		}),
+		archived_at: z.iso
 			.datetime()
 			.nullable()
 			.optional()
@@ -57,17 +50,11 @@ export const UpdateAnnouncementBodySchema = z
 		author: z.string().optional().openapi({ example: "ReVanced" }),
 		title: z.string().openapi({ example: "Welcome" }),
 		content: z.string().optional().openapi({ example: "Some content" }),
-		created_at: z
-			.string()
-			.datetime()
-			.nullable()
-			.optional()
-			.openapi({
-				example: "2024-01-01T00:00:00.000Z",
-				description: "UTC timestamp.",
-			}),
-		archived_at: z
-			.string()
+		created_at: z.iso.datetime().nullable().optional().openapi({
+			example: "1970-01-01T00:00:00.000Z",
+			description: "UTC timestamp.",
+		}),
+		archived_at: z.iso
 			.datetime()
 			.nullable()
 			.optional()
